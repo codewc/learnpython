291e771966d08d3bc547d7b8632f72debba079bb
wangchun <wangchun@192.168.72.1>
2018-03-16 00:56:06 +0800

    增加功能模块

--- learn/hello/urls.py
+++ learn/hello/urls.py
@@ -15,14 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
+'''
+The tutorial project has just one app, polls. In real Django projects, there might be five, ten, twenty apps or more. How does Django differentiate the URL names between them? For example, the polls app has a detail view, and so might an app on the same project that is for a blog. How does one make it so that Django knows which app view to create for a url when using the {% url %} template tag?
+
+The answer is to add namespaces to your URLconf. In the polls/urls.py file, go ahead and add an app_name to set the application namespace:
+like: app_name = 'polls'
+<li><a href="{% url 'polls:detail' question.id %}">{{ question.question_text }}</a></li>
+'''
+app_name = 'hello'
 urlpatterns = [
     #ex: /hello/
     path('', views.index, name='index'),
     #ex /helllo/5/
+    # the 'name' value as called by the {% url %} template tag
     path('<int:querstion_id>/',views.detail,name='detail'),
     path('<int:querstion_id>/results/',views.reslult,name='results'),
     path('<int:querstion_id>/vote/',views.vote,name='vote'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- learn/hello/views.py
+++ learn/hello/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.http import Http404
+from django.urls import reverse
+
+
 from django.template import loader
-from .models import Question
+from .models import Question,Choice
 # Create your views here.
 '''
 def index(request):
@@ -10,6 +14,8 @@ def index(request):
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output) 
 '''
+
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_data')[:5]
     template = loader.get_template('hello/index.html')
@@ -17,15 +23,50 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+'''
+
+def index(request):
+    latest_querstion_list = Question.objects.order_by('-pub_data')[:5]
+    context = {'latest_question_list':latest_querstion_list}
+    return render(request, 'hello/index.html', context)
 
 def reslult(request, querstion_id):
     response = "you,re looking at the reslult of quersion"+str(querstion_id)
     return HttpResponse(response)
-
+'''
 def detail(request, querstion_id):
     response = "you,re looking at the detail of quersion" + str(querstion_id)
     return HttpResponse(response)
 
+'''
+def detail(request,querstion_id):
+    try:
+        question = Question.objects.get(pk=querstion_id)
+    except Question.DoesNotExist:
+        raise Http404('Querstion does not exist')
+    context = {'question':question}
+    return render(request, 'hello/detail.html', context)
+'''
 def vote(request,querstion_id):
     response = "you,re voting on quersion" + str(querstion_id)
     return HttpResponse(response)
+
+'''
+
+def vote(request, querstion_id):
+    question = get_object_or_404(Question, pk=querstion_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'hello/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('hello:results', args=(question.id,)))
